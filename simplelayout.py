a92863164ab243d9ca81117852823cb067260d01
KomorebiTso <Komorebi0216@163.com>
2020-11-11 11:51:45 +0800

    finish

--- simplelayout.py
+++ simplelayout.py
@@ -9,8 +9,8 @@ def main():
     parser.add_argument('--unit_grid', type=int, help='矩形组件分辨率')
     parser.add_argument('--unit_n', type=int, help='组件数')
     parser.add_argument('--positions', type=int, nargs='*', help='矩形组件分辨率')
-    parser.add_argument('-o', '--outdir', type=str, default='example_dir', help='输出目录呀')
-    parser.add_argument('--file_name', type=str, default='example', help='输出文件名哟')
+    parser.add_argument('-o', '--outdir', type=str, default='example_dir', help='输出目录')
+    parser.add_argument('--file_name', type=str, default='example', help='输出文件名')
     args = parser.parse_args()
 
     if not os.path.exists(args.outdir):
@@ -22,15 +22,15 @@ def main():
         pass
 
     if args.board_grid % args.unit_grid != 0:
-        print('布局板分辨率必须整除组件分辨率')
+        print('布局板分辨率须整除组件分辨率')
         sys.exit(1)
 
     if len(args.positions) != args.unit_n:
         sys.exit(1)
 
-    MAX = max(args.positions)
-    MIN = min(args.positions)
-    if not (MIN >= 1 and MAX <= int(args.board_grid / args.unit_grid) ** 2):
+    Max = max(args.positions)
+    Min = min(args.positions)
+    if not (Min >= 1 and Max <= int(args.board_grid / args.unit_grid) ** 2):
         sys.exit(1)
 
 
